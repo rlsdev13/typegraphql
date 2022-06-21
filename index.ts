8a7b579b7cef7d51f049e9d926acfc03ed7e8b07
Rafael Ledesma <rledesma@softengy.com>
2022-06-21 16:03:49 -0500

    index

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 import "reflect-metadata";
+import { ApolloServer } from 'apollo-server';
+import * as path from 'path';
+import { buildSchema } from 'type-graphql';
+
+async function bootstrap(){
+    const schema = await buildSchema({
+        resolvers: [],
+        emitSchemaFile: path.resolve(__dirname,"schema.gql"),
+    })
+}
